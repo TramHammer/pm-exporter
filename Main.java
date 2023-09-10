8336307398533a3085c9f867e83619e773b23fc6
oog <78754673+TramHammer@users.noreply.github.com>
2023-09-10 17:49:35 -0400

    Inital

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+
+
+public class Main {
+    static FileManagement file = new FileManagement();
+    public static void main(String[] args) {
+        file.readConfig();
+        file.copyFiles();
+    }
+}
